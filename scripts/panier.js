f9cb7310c627bf52a3e1e226a2fb40b00ea674cb
philippepecoraro <philippe.pecoraro@bbox.fr>
2020-09-16 20:18:45 +0200

    Ajout formulaire

--- scripts/panier.js
+++ scripts/panier.js
@@ -9,9 +9,9 @@ const produitPanier = document.getElementById("contenu-donnees");
               <div class="nomModele"><h2>Modèle:<br/> ${panier.nom}</h2></div>
               <div class="lentillesModele"><p>Objectif(s): ${panier.lentilles}</p></div>             
               <div class="descriptionModele"><p>Description: ${panier.description}</p></div>
-              <div class="prixModele"><p>Prix: ${panier.prix}€</p></div>
+              <div class="prixModele"><p><strong>Prix: ${panier.prix}€</strong></p></div>
               <div class="quantiteModele"><p>Quantité: ${panier.quantite}</p></div>
-              <div class="prixTotal"><p>Prix total modèle(s) par quantité: <br/>${panier.prix * panier.quantite}€</p></div>
+              <div class="prixTotal"><p><strong>Prix total modèle(s) par quantité: <br/>${panier.prix * panier.quantite}€</strong></p></div>
             </div>                            
           </div>          
       </article> `;
@@ -23,8 +23,56 @@ const produitPanier = document.getElementById("contenu-donnees");
       console.log(reduced);
       console.log(typeof reduced);
       const finalCheck = document.getElementById('prix-total-final');
-      finalCheck.innerHTML =  'Prix total final:' + reduced + '€';
-}  
+      finalCheck.innerHTML = `<p><strong>Prix total final: ${reduced}€</strong></p>`;
+
+         
+    //On récupère les données du formulaire lors du click sur le bouton
+    const ajouterContact = document.getElementById('formulaire');
+    ajouterContact.addEventListener('submit', function(event) {
+      event.preventDefault();
+      let firstName = document.getElementById('prenom').value;
+     // console.log(firstName);
+      let lastName = document.getElementById('nom').value;
+      let address = document.getElementById('adresse').value;
+      let city = document.getElementById('ville').value;
+      let email = document.getElementById('email').value;    
+     let contact = {
+       firstName: firstName,
+       lastName: lastName,
+       adress: address,
+       city: city,
+       email: email,
+     };
+      console.log(contact);
+    //  console.log(typeof contact);
+    //  console.log(Array.isArray(contact));
+      //On crée un tableau de products
+      const products = [];
+      //On ajoute dans le tableau
+      contenuPanier.forEach(panier => {
+        products.push(panier.id);
+    })
+      console.log(products);
+      console.log(typeof products);
+      console.log(Array.isArray(products));
+
+       async function postContactProduct() {
+            let response = await fetch("http://localhost:3000/api/cameras/order" , {
+            method: 'POST',
+            body:{contact},
+            headers: new Headers({ 'Content-Type': 'application/json'}),  
+        
+       });       
+        if(response.ok) {
+          let responseData = await response.json();
+      //    console.log(responseData);
+        } else {
+        console.error("Status: " + response.status);
+        }
+    }  
+    postContactProduct();      
+  });
+} 
  //Mise à zéro du panier
   let init = document.getElementById('boutonInit');
     init.addEventListener('click', function(e) {
